996c1907d0bc116dcb954275a065840cd535257f
Sai Chandan Mohanty <76147965+scarface68@users.noreply.github.com>
2024-02-06 00:22:10 +0530

    Update HomeContainer.js

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -25,7 +25,7 @@ export default function HomeContainer({ onSignup, onSignin }) {
         <Wrapper justify="space-between" minHeight="600px">
           <HeroSection direction="column">
             <HeroTitle>Kanban.</HeroTitle>
-            <HeroBody>Kraftbase Productions</HeroBody>
+            <HeroBody>Scarface68 Productions</HeroBody>
           </HeroSection>
           <OnboardingForm onSignup={onSignup} onSignin={onSignin} />
         </Wrapper>
